460f450e8cbe2bb31b9f1e56ff52c8845367a220
csn3985 <80070819+csn3985@users.noreply.github.com>
2021-03-06 18:47:20 -0500

    Delete run.sh

--- run.sh
+++
@@ -1,6 +0,0 @@
-gnome-terminal -x sh -c "docker run -it --rm -v ${PWD}/ui/ui/:/app -p 3001:3000 -e CHOKIDAT_USEPOLLING=true three_tier-ui:latest; bash"
-#gnome-terminal -x sh -c "docker run -it --rm -p 3001:3000 -e CHOKIDAT_USEPOLLING=true three_tier-ui:latest; bash"
-gnome-terminal -x sh -c "docker run -it --rm -p 5001:5000 three_tier-bl:latest; bash"
-
-#gnome-terminal -x sh -c "docker run -it --rm -p 0.0.0.0:3307:3306 -e MYSQL_ROOT_PASSWORD=password -e MYSQL_DATABASE=mysql -d mysql:latest"
-
