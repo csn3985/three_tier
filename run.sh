abf980d833791780339e7f17c05caedbf1dc0d80
csn3985 <80070819+csn3985@users.noreply.github.com>
2021-03-28 22:43:35 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-gnome-terminal -x sh -c "docker run -it --rm -v ${PWD}/ui/ui/:/app -p 3001:3000 -e CHOKIDAT_USEPOLLING=true three_tier-ui:latest; bash"
+gnome-terminal -x sh -c "docker run -it --rm -v ${PWD}/ui/ui/:/app -p 3001:3000 -e CHOKIDAT_USEPOLLING=true three_tier-ui:latest; bas"
 #gnome-terminal -x sh -c "docker run -it --rm -p 3001:3000 -e CHOKIDAT_USEPOLLING=true three_tier-ui:latest; bash"
 gnome-terminal -x sh -c "docker run -it --rm -p 5001:5000 three_tier-bl:latest; bash"
 
