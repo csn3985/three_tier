29e5dd9919eb4c27019e2a2bdfaa08bc8e05fec3
csn3985 <80070819+csn3985@users.noreply.github.com>
2021-03-28 22:46:05 -0400

    Update data.sql

--- db/data.sql
+++ db/data.sql
@@ -1,7 +1,7 @@
 CREATE DATABASE courses;
 USE courses;
 CREATE TABLE courses (ID INT(30), NAME VARCHAR(20), RATING INT(20), NUMRATINGS INT(20), COMMENTS VARCHAR(10000));
-INSERT INTO courses VALUES(1, 'CS150', 2, 4, 'comment1: This was a good class');
+INSERT INTO courses VALUES(1, 'CS1501', 2, 4, 'comment1: This was a good class');
 INSERT INTO courses VALUES(2, 'CS250', 3, 5, 'This was a terrible class');
 INSERT INTO courses VALUES(3, 'CS350', 5, 3, 'The professor was informative');
 INSERT INTO courses VALUES(4, 'M150', 5, 3, 'The professor never showed up');
