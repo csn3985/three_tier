91aaeb472d7c04e84d4d9936f0f276e306fe0ab3
csn3985 <80070819+csn3985@users.noreply.github.com>
2021-03-28 22:46:25 -0400

    Update data.sql

--- db/data.sql
+++ db/data.sql
@@ -4,6 +4,6 @@ CREATE TABLE courses (ID INT(30), NAME VARCHAR(20), RATING INT(20), NUMRATINGS I
 INSERT INTO courses VALUES(1, 'CS1501', 2, 4, 'comment1: This was a good class');
 INSERT INTO courses VALUES(2, 'CS250', 3, 5, 'This was a terrible class');
 INSERT INTO courses VALUES(3, 'CS350', 5, 3, 'The professor was informative');
-INSERT INTO courses VALUES(4, 'M150', 5, 3, 'The professor never showed up');
+INSERT INTO courses VALUES(4, 'M150', 5, 3, 'The professor never showd up');
 INSERT INTO courses VALUES(5, 'M151', 5, 3, 'I liked the final project');
 INSERT INTO courses VALUES(6, 'M200', 5, 3, 'I dropped this course');
