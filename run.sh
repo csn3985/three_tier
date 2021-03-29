da477ddca65a13594037656b2778e6a890cdd6b0
csn3985 <80070819+csn3985@users.noreply.github.com>
2021-03-28 22:48:20 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,5 +3,5 @@ gnome-terminal -x sh -c "docker run -it --rm -v ${PWD}/ui/ui/:/app -p 3001:3000 
 gnome-terminal -x sh -c "docker run -it --rm -p 5001:5000 three_tier-bl:latest; bash"
 
 #gnome-terminal -x sh -c "docker run -it --rm -p 0.0.0.0:3307:3306 -e MYSQL_ROOT_PASSWORD=password -e MYSQL_DATABASE=mysql -d mysql:latest"]
-#
+
 
