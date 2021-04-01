8195c00f182b2ab4831c2dfeccb118ad5ed82fae
Ubuntu <ubuntu@ip-172-31-16-62.us-east-2.compute.internal>
2021-04-01 17:34:29 +0000

    Can now post comments.

--- bl/three_tier-bl.py
+++ bl/three_tier-bl.py
@@ -1,7 +1,7 @@
 import mysql.connector
 
 import flask
-from flask import request, jsonify
+from flask import request, jsonify, redirect, url_for
 from flask_cors import CORS, cross_origin
 
 
@@ -14,8 +14,6 @@ app.config["DEBUG"] = True
 @app.route('/api/v1/resources/courses/<id>', methods=['GET'])
 @cross_origin()
 def get_course(id):
-    print( "Ass")
-    print(request.get_data())
     courseID = id
     data = []
     try:
@@ -48,37 +46,9 @@ def api_all():
         cnx.close()
     return jsonify(courses)
 
-@app.route('/api/v1/resources/comments/<id>', methods=['GET', 'POST'])
+@app.route('/api/v1/resources/comments/<id>', methods=['GET'])
 def comments(id):
     comments = []
-    if request.method == 'POST':
-        courseID = request.form['id']
-        comment = request.form['comment']
-        try:
-            cnx = mysql.connector.connect(user='root', password='password', host='db', database='courses', port=3306);
-
-            query = "INSERT INTO comments(COURSE_ID, COMMENT) VALUES(" + courseID + ", '" + comment + "')"
-            cursor= cnx.cursor()
-            cursor.execute(query)
-            cnx.commit()
-            cursor.close()
-            cursor= cnx.cursor()
-            query = "SELECT * FROM comments WHERE COURSE_ID=" + courseID
-            cursor.execute(query)
-
-            comments = []
-
-            for (id, course_id, comment, date)  in cursor:
-                comments.append({'date': date, 'comment': comment})
-
-            cursor.close()
-            cnx.close()
-        except mysql.connector.Error as err:
-            print(err)
-        else:
-            cnx.close()
-
-        return jsonify(comments)
     if request.method == 'GET':
         courseID = id
         try:
@@ -101,4 +71,32 @@ def comments(id):
             cnx.close()
 
         return jsonify(comments)
+@app.route('/api/v1/resources/comments/', methods=['POST'])
+def post_comment():
+    comments = []
+    courseID = request.json['id']
+    comment = request.json['comment']
+    try:
+        cnx = mysql.connector.connect(user='root', password='password', host='db', database='courses', port=3306);
+
+        query = "INSERT INTO comments(COURSE_ID, COMMENT) VALUES(" + courseID + ", '" + comment + "')"
+        cursor= cnx.cursor()
+        cursor.execute(query)
+        cnx.commit()
+        cursor.close()
+        cursor= cnx.cursor()
+        query = "SELECT * FROM comments WHERE COURSE_ID=" + courseID
+        cursor.execute(query)
+
+        comments = []
+
+        for (id, course_id, comment, date)  in cursor:
+            comments.append({'date': date, 'comment': comment})
+        cursor.close()
+        cnx.close()
+    except mysql.connector.Error as err:
+        print(err)
+    else:
+        cnx.close()
+    return jsonify(comments)
 app.run(host='0.0.0.0')
--- ui/ui/src/ViewCourse.js
+++ ui/ui/src/ViewCourse.js
@@ -1,6 +1,38 @@
 import React, {Component} from 'react';
 import Comments from './comments';
 
+
+class CommentBox extends Component {
+  constructor(props) {
+    super(props);
+	  console.log(props);
+    this.state = {
+      inputValue: ''
+    };
+  }
+
+  render() {
+    return (
+	    <div>
+            <input value={this.state.inputValue} onChange={evt => this.updateInputValue(evt)}/><br/>
+	    <button type="button" onClick={e =>this.handleClick(e)}>Submit</button>
+	    </div>
+    );
+  }
+
+
+  updateInputValue(evt) {
+    this.setState({
+      inputValue: evt.target.value
+    });
+  }
+  handleClick(e) {
+		console.log(this.state.inputValue);
+                fetch('http://' + process.env.REACT_APP_PUB_IP + ':5001/api/v1/resources/comments/', {method:"POST", headers: { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin' : 'application/json'}, body: JSON.stringify({id: this.props.id, comment: this.state.inputValue})})
+                .catch(console.log)
+	}
+};
+
 export default class ViewCourse extends Component {
 	state = {
                 courses: [],
@@ -30,7 +62,10 @@ export default class ViewCourse extends Component {
 		<div>
 		<center><h1 class="title">{this.state.courses[0]}</h1></center>
 		</div>
+		<div>
 		<Comments comments = {this.state.comments} />
+		<CommentBox id={this.props.match.params.id}/>
+		</div>
 		</div>
 	)
 	}
